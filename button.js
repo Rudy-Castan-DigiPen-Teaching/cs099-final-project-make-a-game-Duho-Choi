fa0911a91e70bde7108da241e611928770e3d3d0
hoduchoi7677 <79897462+hoduchoi7677@users.noreply.github.com>
2021-06-08 14:39:30 +0900

    made buttons

--- button.js
+++ button.js
@@ -11,24 +11,44 @@ class button
         this.y = y_pos;
         this.width = width;
         this.height = height;
-        this.value = 0;
     }
 
-    draw()
+    draw(txt)
     {
+        const mouseInButton = (mouseX > this.x - this.width / 2 && mouseX < this.x + this.width / 2) && (mouseY > this.y - this.height / 2 && mouseY < this.y + this.height / 2);
+
         push();
         rectMode(CENTER);
+        if(mouseInButton == true)
+        {
+            stroke('#ffcc66');
+            fill('#ff9933');
+        }
+        else
+        {
+            stroke('#ff9933');
+            fill('#ffcc66');
+        }
+        strokeWeight(5);
         rect(this.x,this.y,this.width,this.height);
+        pop();
+        push();
+        textAlign(CENTER,CENTER);
+        textSize(this.height * 3/4);
+        text(txt,this.x,this.y);
         pop();
     }
 
-    click()
+    clicked()
     {
-        const mouseInButton = (mouseX > this.x && mouseX < this.x + this.width) && (mouseY > this.Y && mouseY < this.y + this.button);
-
-        if(mouseIsPressed && mouseInButton && this.value == 0)
+        const mouseInButton = (mouseX > this.x - this.width / 2 && mouseX < this.x + this.width / 2) && (mouseY > this.y - this.height / 2 && mouseY < this.y + this.height / 2);
+        
+        if(mouseIsPressed && mouseInButton)
         {
-            
+            return true;
+        }
+        else{
+            return false;
         }
     }
 }
--- sketch.js
+++ sketch.js
@@ -5,8 +5,16 @@
 
 const main_menu = 0;
 const game_screen = 1;
+const help_screen = 2;
+const options_screen = 11;
+const credits_screen = 12;
 
 let current_screen = 0;
+
+let start_button;
+let options_button;
+let credits_button;
+let main_button;
 
 let player;
 let player_laser = [];
@@ -22,40 +30,96 @@ function preload()
 
 function setup()
 {
-    createCanvas( 1200, 800 );
+    createCanvas( 1200, 900 );
+
+    // buttons
+    start_button = new button(width/2, height/2 + 100, 300, 60);
+    options_button = new button(width/2, height/2 + 200, 300, 60);
+    credits_button = new button(width/2, height/2 + 300, 300, 60);
+    main_button = new button(width/13,height/12,50,50);
+
+    // player & enemies
     player = new spaceship(width/2, height/2, 0);
+    
 }
 
 function draw()
 {
     background( 20 );
-    player.update();
-    player.draw();
+    switch(current_screen)
+    {
+        case main_menu:
+            // draw buttons
+            start_button.draw('START');
+            options_button.draw('OPTIONS');
+            credits_button.draw('CREDITS');
 
-    if(keyIsDown(32))
-    {
-        if((millis() / 1000 - timer) % 2 == 0)
-        {
-        player_laser.push(new laser(player));
-        }
-    }
-    else
-    {
-        
+            // change screens when button clicked
+            if(start_button.clicked())
+            {
+                current_screen = help_screen;
+            }
+            else if(options_button.clicked())
+            {
+                current_screen = options_screen;
+            }
+            else if(credits_button.clicked())
+            {
+                current_screen = credits_screen;
+            }
+
+            break;
+        case help_screen:
+
+            break;
+        case options_screen:
+
+            break;
+        case credits_screen:
+
+            break;
+        case game_screen:
+            player.update();
+            player.draw();
+
+            if(keyIsDown(32))
+            {
+                if(((floor(millis()/10) - shoot_timer) % 20 == 0) && do_not_shoot == false)
+                {
+                player_laser.push(new laser(player));
+                }
+            }
+            else
+            {
+                
+            }
+            
+            // player lasers update
+            for(let bullet of player_laser)
+            {
+                bullet.update();
+            }
+            
+            
+            for(let i = player_laser.length - 1; i >= 0; --i)
+            {
+                let particle = player_laser[i]
+                if(particle.position.x > width || particle.position.x < 0 ||
+                    particle.position.y > height || particle.position.y < 0)
+                {
+                    player_laser.splice(i,1);
+                }
+            }
+            break;
+
     }
 
-    for(let bullet of player_laser)
+    if(current_screen != main_menu && current_screen != game_screen)
     {
-        bullet.update();
-    }
-    
-    for(let i = player_laser.length - 1; i >= 0; --i)
-    {
-        let particle = player_laser[i]
-        if(particle.position.x > width || particle.position.x < 0 ||
-            particle.position.y > height || particle.position.y < 0)
+        main_button.draw('◁');
+        if(main_button.clicked())
         {
-            player_laser.splice(i,1);
+            current_screen = main_menu;
         }
     }
 }
@@ -64,6 +128,6 @@ function keyPressed()
 {
     if(keyCode == 32)
     {
-        timer = millis() / 1000;
+        shoot_timer = floor(millis()/10);
     }
 }
